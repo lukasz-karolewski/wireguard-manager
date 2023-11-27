84754160d72c103e42d0cc416a0868abace7f031
Lukasz Karolewski <lkarolewski@gmail.com>
2023-11-27 06:57:56 +0000

    proper props for input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,14 +1,12 @@
-type InputProps = {
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   name: string;
   register: any;
-};
+}
 
-const Input = ({ name, register }: InputProps) => (
+export const Input = ({ name, register }: InputProps) => (
   <input
     className="border px-3 py-2 text-gray-900"
     type="text"
     {...register(name, { required: true })}
   />
 );
-
-export default Input;
