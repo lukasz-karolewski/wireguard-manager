1c760c919fc6906c569031df33304c5bc4ac6c24
Lukasz Karolewski <lkarolewski@gmail.com>
2023-12-10 08:32:00 +0000

    Refactor AddEditSiteModal component layout

--- src/components/app/AddEditSiteModal.tsx
+++ src/components/app/AddEditSiteModal.tsx
@@ -82,78 +82,93 @@ export const AddEditSiteModal = NiceModal.create<Props>(({ site }) => {
       className="w-2/3"
     >
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="p-4">
-          <FormField label="Name">
-            <Input type="text" {...register("name", { required: true })} />
-          </FormField>
+        <div className="flex flex-col gap-4 p-4">
+          <div className="border border-gray-300">
+            <h3 className="bg-accent p-4 text-white">Interface</h3>
+            <div className="p-4">
+              <FormField label="Name">
+                <Input type="text" {...register("name", { required: true })} />
+              </FormField>
 
-          <FormField label="Site Id" help="Used to assign a /24 network ">
-            <Input
-              type="number"
-              min={1}
-              max={255}
-              disabled={!isAdd}
-              {...register("id", { required: true, valueAsNumber: true, min: 1, max: 255 })}
-            />
-          </FormField>
+              <FormField label="Site Id" help="Used to assign a /24 network ">
+                <Input
+                  type="number"
+                  min={1}
+                  max={255}
+                  disabled={!isAdd}
+                  {...register("id", { required: true, valueAsNumber: true, min: 1, max: 255 })}
+                />
+              </FormField>
 
-          <FormField label="Endpoint" help="External ip or DNS">
-            <Input {...register("endpointAddress", { required: true })} />
-          </FormField>
+              <FormField label="Listen Port">
+                <Input
+                  type="number"
+                  {...register("listenPort", {
+                    required: false,
+                    min: 1024,
+                    max: 65535,
+                    valueAsNumber: true,
+                  })}
+                />
+              </FormField>
 
-          <FormField
-            label="Local Networks"
-            help="Local networks to expose to other sites, and clients"
-          >
-            <Input {...register("localAddresses", { required: false })} />
-          </FormField>
+              <FormField label="Private Key">
+                <Input
+                  placeholder="leave empty to generate key"
+                  {...register("private_key", { required: false })}
+                />
+              </FormField>
 
-          <FormField label="DNS address" help="optional, can be used by clients">
-            <Input {...register("dns", { required: false })} />
-          </FormField>
+              <FormField label="Post Up script">
+                <Input {...register("postUp", { required: false })} />
+              </FormField>
 
-          <FormField label="Pihole address" help="optional, can be used by clients">
-            <Input {...register("dns_pihole", { required: false })} />
-          </FormField>
+              <FormField label="Post Down script">
+                <Input {...register("postDown", { required: false })} />
+              </FormField>
+            </div>
+          </div>
+          <div className="border border-gray-300">
+            <h3 className="bg-accent p-4 text-white">Peer options</h3>
+            <div className="p-4">
+              <FormField label="Endpoint" help="External IP or DNS where should clients connect">
+                <Input {...register("endpointAddress", { required: true })} />
+              </FormField>
+              <FormField
+                label="Local Networks"
+                help="Local networks to expose to other sites, and clients"
+              >
+                <Input {...register("localAddresses", { required: false })} />
+              </FormField>
 
-          <FormField label="Config Path">
-            <Input {...register("config_path", { required: false })} />
-          </FormField>
+              <FormField label="DNS IP" help="optional, can be used by clients">
+                <Input {...register("dns", { required: false })} />
+              </FormField>
 
-          <FormField label="Listen Port">
-            <Input
-              type="number"
-              {...register("listenPort", {
-                required: false,
-                min: 1024,
-                max: 65535,
-                valueAsNumber: true,
-              })}
-            />
-          </FormField>
+              <FormField label="Pihole IP" help="optional, can be used by clients">
+                <Input {...register("dns_pihole", { required: false })} />
+              </FormField>
+            </div>
+          </div>
+          <div className="border border-gray-300">
+            <h3 className="bg-accent p-4 text-white">App options</h3>
+            <div className="p-4">
+              <FormField label="Config Path">
+                <Input {...register("config_path", { required: false })} />
+              </FormField>
 
-          <FormField label="Post Up script">
-            <Input {...register("postUp", { required: false })} />
-          </FormField>
-
-          <FormField label="Post Down script">
-            <Input {...register("postDown", { required: false })} />
-          </FormField>
-
-          <FormField label="Private Key">
-            <Input {...register("private_key", { required: false })} />
-          </FormField>
-
-          <FormField
-            label="Mark as Default Site"
-            help="Clients will display config for this site by default"
-          >
-            <Input
-              type="checkbox"
-              className="w-4"
-              {...register("markAsDefault", { required: false })}
-            />
-          </FormField>
+              <FormField
+                label="Mark as Default Site"
+                help="Clients will display config for this site by default"
+              >
+                <Input
+                  type="checkbox"
+                  className="w-4"
+                  {...register("markAsDefault", { required: false })}
+                />
+              </FormField>
+            </div>
+          </div>
         </div>
         <div className="flex justify-end gap-4 bg-slate-100 p-4 ">
           <Button type="submit">{isAdd ? "Add" : "Save"}</Button>
